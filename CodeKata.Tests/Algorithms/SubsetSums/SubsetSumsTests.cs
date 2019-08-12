a9bafc064419e0a7697c57cac1fe443acb20ed03
Luca Mazzanti <lucamazzanti83@gmail.com>
2019-08-12 14:39:47 +0200

    added a new algorithm: subset sums.

---
+++ CodeKata.Tests/Algorithms/SubsetSums/SubsetSumsTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKata.Tests.Algorithms.SubsetSums
+{
+    public class SubsetSumsTests
+    {
+        public static object[] UseCases = new object[]
+        {
+            new object[] { 1, new int[] { 1 }, new int[][] {
+                new int[] { 1 }
+            } },
+            new object[] { 2, new int[] { 1, 2 }, new int[][] {
+                new int[] { 1, 1 },
+                new int[] { 2 }
+            } },
+            new object[] { 5, new int[] { 1, 2, 3 }, new int[][] {
+                new int[] { 3, 2 },
+                new int[] { 3, 1, 1 },
+                new int[] { 2, 2, 1 },
+                new int[] { 2, 1, 1, 1 },
+                new int[] { 1, 1, 1, 1, 1 }
+            } },
+        };
+
+        [Test]
+        [TestCaseSource(nameof(UseCases))]
+        public void AllSumsUnordered_UseCases(int sum, int[] numbers, int[][] expectedResult)
+        {
+            var result = CodeKata.Algorithms.SubsetSums.SubsetSums.AllSumsUnordered(sum, numbers);
+            CollectionAssert.AreEquivalent(expectedResult, result);
+        }
+    }
+}
---
+++ CodeKata/Algorithms/SubsetSums/SubsetSums.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeKata.Algorithms.SubsetSums
+{
+    public class SubsetSums
+    {
+        public static int[][] AllSums(int total, int[] items)
+        {
+            return AllSums(total, items, new int[] { }, new int[][] { });
+        }
+
+        private static int[][] AllSums(int total, int[] items, int[] row, int[][] result)
+        {
+            if (total == 0) result = result.Concat(new int[][] { row }).ToArray();
+            if (total <= 0) return result;
+
+            foreach (var item in items.OrderBy(i => i))
+                result = AllSums(total - item, items, row.Concat(new[] { item }).ToArray(), result);
+
+            return result;
+        }
+
+        public static int[][] AllSumsUnordered(int total, int[] items)
+        {
+            return AllSumsUnordered(total, items, new int[] { }, new int[][] { });
+        }
+
+        private static int[][] AllSumsUnordered(int total, int[] items, int[] row, int[][] result)
+        {
+            if (total == 0) result = result.Concat(new int[][] { row }).ToArray();
+            if (total <= 0) return result;
+
+            foreach (var item in items.OrderByDescending(i => i))
+                result = AllSumsUnordered(
+                    total - item,
+                    items.Where(i => i <= item).ToArray(),
+                    row.Concat(new[] { item }).ToArray(),
+                    result);
+
+            return result;
+        }
+    }
+}
