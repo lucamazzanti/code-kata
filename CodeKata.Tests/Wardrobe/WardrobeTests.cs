a2a14d06ffad64e18e9f55ddbadfd655d81159fa
Luca Mazzanti <lucamazzanti83@gmail.com>
2019-08-12 09:42:28 +0200

    wardrobe kata: first test

---
+++ CodeKata.Tests/Wardrobe/WardrobeTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKata.Tests.Wardrobe
+{
+    public class WardrobeTests
+    {
+        [Test]
+        public void Customize_SingleElementThatFit_ReturnsThatCombination()
+        {
+            var wardrobe = new CodeKata.Wardrobe.Wardrobe();
+            var result = wardrobe.Customize(1, 1);
+            Assert.AreEqual(1, 1);
+        }
+
+        //[Test]
+        //public void Customize_TwoElementsThatFit_ReturnsTwoCombination()
+        //{
+        //    var wardrobe = new CodeKata.Wardrobe.Wardrobe();
+        //    var result = wardrobe.Customize(2, new int[] { 1, 2 });
+        //    Assert.AreEqual(1, 1);
+        //}
+    }
+}
---
+++ CodeKata/Wardrobe/Wardrobe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKata.Wardrobe
+{
+    public class Wardrobe
+    {
+        public int Customize(int size, int element)
+        {
+            return 1;
+        }
+    }
+}
