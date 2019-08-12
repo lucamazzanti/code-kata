df78703bdd4fb95a768afe93434e3d09c418fe9f
Luca Mazzanti <lucamazzanti83@gmail.com>
2019-08-12 10:02:11 +0200

    reordered tests

--- CodeKata.Tests/Wardrobe/WardrobeTests.cs
+++ CodeKata.Tests/Wardrobe/WardrobeTests.cs
@@ -8,30 +8,6 @@ namespace CodeKata.Tests.Wardrobe
     public class WardrobeTests
     {
         [Test]
-        public void Customize_SingleElementThatFit_ReturnsThatCombination()
-        {
-            var wardrobe = new CodeKata.Wardrobe.Wardrobe();
-            var result = wardrobe.Customize(1, new uint[] { 1 });
-            CollectionAssert.AreEqual(new uint[][] { new uint[] { 1 } }, result);
-        }
-
-        [Test]
-        public void Customize_TwoElementsThatFit_ReturnsTwoCombination()
-        {
-            var wardrobe = new CodeKata.Wardrobe.Wardrobe();
-            var results = wardrobe.Customize(2, new uint[] { 1, 2 });
-            CollectionAssert.AreEqual(new uint[][] { new uint[] { 2 }, new uint[] { 1, 1 } }, results);
-        }
-
-        [Test]
-        public void Customize_NoElementThatFit_ReturnsEmpty()
-        {
-            var wardrobe = new CodeKata.Wardrobe.Wardrobe();
-            var result = wardrobe.Customize(1, new uint[] { 2 });
-            CollectionAssert.AreEqual(new uint[][] { }, result);
-        }
-
-        [Test]
         public void Customize_InexistentRoomSize_ThrowsArgumentException()
         {
             var wardrobe = new CodeKata.Wardrobe.Wardrobe();
@@ -60,5 +36,32 @@ namespace CodeKata.Tests.Wardrobe
                 wardrobe.Customize(1, null);
             });
         }
+
+        [Test]
+        public void Customize_NoElementThatFit_ReturnsEmpty()
+        {
+            var wardrobe = new CodeKata.Wardrobe.Wardrobe();
+            var result = wardrobe.Customize(1, new uint[] { 2 });
+            CollectionAssert.AreEqual(new uint[][] { }, result);
+        }
+
+        [Test]
+        public void Customize_SingleElementThatFit_ReturnsThatCombination()
+        {
+            var wardrobe = new CodeKata.Wardrobe.Wardrobe();
+            var result = wardrobe.Customize(1, new uint[] { 1 });
+            CollectionAssert.AreEqual(new uint[][] { new uint[] { 1 } }, result);
+        }
+
+        [Test]
+        public void Customize_TwoElementsThatFit_ReturnsTwoCombination()
+        {
+            var wardrobe = new CodeKata.Wardrobe.Wardrobe();
+            var results = wardrobe.Customize(2, new uint[] { 1, 2 });
+            CollectionAssert.AreEqual(new uint[][] { new uint[] { 2 }, new uint[] { 1, 1 } }, results);
+        }
+
+
+
     }
 }
